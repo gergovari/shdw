1fa5f421f6f333510e886ad1f857a9bf582910c6
Gergo Vari <work@gergovari.com>
2025-03-02 15:04:15 +0100

    initial: commit

---
+++ app/run.sh
@@ -0,0 +1,1 @@
+west build -p -b esp_wrover_kit/esp32/procpu && west flash && west espressif monitor
---
+++ app/src/main.c
@@ -0,0 +1,31 @@
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/display.h>
+#include <zephyr/drivers/gpio.h>
+#include <lvgl.h>
+#include <stdio.h>
+#include <string.h>
+
+#define LOG_LEVEL CONFIG_LOG_DEFAULT_LEVEL
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(app);
+
+int main(void)
+{
+	const struct device *display_dev;
+	lv_obj_t *hello_world_label;
+
+	display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
+	if (!device_is_ready(display_dev)) {
+		LOG_ERR("Device not ready, aborting test");
+		return 0;
+	}
+
+	hello_world_label = lv_label_create(lv_screen_active());
+	lv_label_set_text(hello_world_label, "Hello world!");
+	lv_obj_align(hello_world_label, LV_ALIGN_CENTER, 0, 0);
+
+	lv_timer_handler();
+	display_blanking_off(display_dev);
+}
