785698e38a7e16a287acc67e799ae8b2ada4c898
Gergo Vari <work@gergovari.com>
2025-07-08 09:01:26 +0200

    feat: native_sim

---
+++ app/nsim-run.sh
@@ -0,0 +1,1 @@
+west build -p -b native_sim/native/64 && west build -t run
--- app/src/main.c
+++ app/src/main.c
@@ -1,4 +1,3 @@
-#include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/display.h>
@@ -6,15 +5,28 @@
 #include <lvgl.h>
 #include <stdio.h>
 #include <string.h>
+#include <zephyr/kernel.h>
+#include <lvgl_input_device.h>
 
 #define LOG_LEVEL CONFIG_LOG_DEFAULT_LEVEL
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(app);
 
+static uint32_t count;
+
+static void lv_btn_click_callback(lv_event_t *e)
+{
+	ARG_UNUSED(e);
+
+	count = 0;
+}
+
 int main(void)
 {
+	char count_str[11] = {0};
 	const struct device *display_dev;
 	lv_obj_t *hello_world_label;
+	lv_obj_t *count_label;
 
 	display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
 	if (!device_is_ready(display_dev)) {
@@ -22,10 +34,34 @@ int main(void)
 		return 0;
 	}
 
-	hello_world_label = lv_label_create(lv_screen_active());
+	if (IS_ENABLED(CONFIG_LV_Z_POINTER_INPUT)) {
+		lv_obj_t *hello_world_button;
+
+		hello_world_button = lv_button_create(lv_screen_active());
+		lv_obj_align(hello_world_button, LV_ALIGN_CENTER, 0, -15);
+		lv_obj_add_event_cb(hello_world_button, lv_btn_click_callback, LV_EVENT_CLICKED,
+				    NULL);
+		hello_world_label = lv_label_create(hello_world_button);
+	} else {
+		hello_world_label = lv_label_create(lv_screen_active());
+	}
+
 	lv_label_set_text(hello_world_label, "Hello world!");
 	lv_obj_align(hello_world_label, LV_ALIGN_CENTER, 0, 0);
 
+	count_label = lv_label_create(lv_screen_active());
+	lv_obj_align(count_label, LV_ALIGN_BOTTOM_MID, 0, 0);
+
 	lv_timer_handler();
 	display_blanking_off(display_dev);
+
+	while (1) {
+		if ((count % 100) == 0U) {
+			sprintf(count_str, "%d", count/100U);
+			lv_label_set_text(count_label, count_str);
+		}
+		lv_timer_handler();
+		++count;
+		k_sleep(K_MSEC(10));
+	}
 }
