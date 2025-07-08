5eb678d785beb492989123f7567d62fb074b01f8
Gergo Vari <work@gergovari.com>
2025-07-08 13:27:18 +0200

    feat: window

--- app/src/main.c
+++ app/src/main.c
@@ -13,9 +13,77 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(app);
 
+char* app_ids[] = {
+	"shd.clock",
+	"shd.notes",
+	"shd.chats",
+	"shd.weather",
+	"shd.maps"
+};
+
+lv_obj_t* root_screen;
+
+void shd_clock(lv_obj_t* cont) {
+	lv_obj_t* clock = lv_label_create(cont);
+
+	lv_label_set_text(clock, "12:34");
+	lv_obj_center(clock);
+}
+
+void shd_dummy(lv_obj_t* cont) {
+	printf("dummy opened.\n");
+}
+
+void (*app_funcs[])(lv_obj_t*) = {
+	shd_clock,
+	shd_dummy,
+	shd_dummy,
+	shd_dummy,
+	shd_dummy
+};
+
+int lv_launcher_launch(lv_obj_t* cont, char* id) {
+	for (unsigned int i = 0; i < sizeof(app_ids)/sizeof(char*); i++) {
+		if (strcmp(app_ids[i], id) == 0) {
+			app_funcs[i](cont);
+			return 0;
+		}
+	}
+	return 1;
+}
+
+void lv_launcher_close_cb(lv_event_t *e) {
+	lv_obj_t* screen = (lv_obj_t*)lv_event_get_user_data(e);
+
+	lv_obj_delete(screen);
+	lv_screen_load(root_screen);
+}
+
+lv_obj_t* lv_launcher_cont_create(lv_obj_t** screen, char* title) {
+	lv_obj_t* window;
+	lv_obj_t* close_btn;
+
+	*screen = lv_obj_create(NULL);
+
+	window = lv_win_create(*screen);
+	lv_win_add_title(window, title);
+
+	close_btn = lv_win_add_button(window, LV_SYMBOL_CLOSE, 40);
+	lv_obj_add_event_cb(close_btn, lv_launcher_close_cb, LV_EVENT_CLICKED, *screen);
+
+	return lv_win_get_content(window);
+}
+
 void lv_launcher_click_cb(lv_event_t *e) {
-	void *data = lv_event_get_user_data(e);
-	printf("%s\n", (char*)data);
+	char* id = (char*)lv_event_get_user_data(e);
+	lv_obj_t* screen;
+	lv_obj_t* cont = lv_launcher_cont_create(&screen, id);
+
+	lv_screen_load(screen);
+	
+	if (lv_launcher_launch(cont, id) != 0) {
+		printf("%s NOT launched.\n", id);
+	}
 }
 
 void lv_launcher(void) {
@@ -27,14 +95,6 @@ void lv_launcher(void) {
 
 	lv_obj_set_size(menu, lv_display_get_horizontal_resolution(NULL), lv_display_get_vertical_resolution(NULL));
 	lv_obj_center(menu);
-	
-	char *app_ids[] = {
-		"clock",
-		"notes",
-		"chats",
-		"weather",
-		"maps"
-	};
 	
 	for (unsigned int i = 0; i < sizeof(app_ids)/sizeof(char*); i++) {
 		cont = lv_menu_cont_create(root);
@@ -62,12 +122,12 @@ int main(void)
 {
 	const struct device* display = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
 	
-	printf("start\n");
 	if (!device_is_ready(display)) {
 		LOG_ERR("Display device not ready!");
 		return 0;
 	}
 	
+	root_screen = lv_screen_active();
 	lv_launcher();
 
 	lv_run(display);
